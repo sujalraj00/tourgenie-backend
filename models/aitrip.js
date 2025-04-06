668c38bd3ea8576253bc925a8e27bdc950b8ccfd
Sujal Raj <sujalraj611@gmail.com>
2025-04-06 15:44:51 +0530

    schema updated

--- models/aitrip.js
+++ models/aitrip.js
@@ -1,33 +1,38 @@
 const mongoose = require('mongoose');
 
 const aitripSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number,
-    passport: String,
-    departure_location: String,
-    destination: String,
-    departure_date: String,
-    return_date: String,
-    airline: String,
-    departure_time: Date,
-    duration_minutes: Number,
-    features: [String],
-    price: Number,
-    hotel: {
-        name: String,
-        link: String,
-        images: [
-            {
-                thumbnail: String,
-                original: String
-            }
-        ],
-        rate_per_night: Number,
-        total_rate: Number
-    },
-    createdAt: Date
-    
+  name: String,
+  email: String,
+  age: Number,
+  passport: String,
+  departure_location: String,
+  destination: String,
+  departure_date: String,
+  return_date: String,
+  flights: [
+    {
+      airline: String,
+      departure_time: Date,
+      duration_minutes: Number,
+      features: [String],
+      price: Number
+    }
+  ],
+  hotels: [
+    {
+      name: String,
+      link: String,
+      images: [
+        {
+          thumbnail: String,
+          original: String
+        }
+      ],
+      rate_per_night: Number,
+      total_rate: Number
+    }
+  ],
+  createdAt: Date
 });
 
 const aiTripModel = mongoose.model("AiTripModel", aitripSchema);
